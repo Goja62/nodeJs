0347f0054ca8c9856f0b386776a549a1cb79812b
Goja <gortnar@beograd.com>
2024-02-14 13:38:18 +0100

    Handling Differenr Routes

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
+app.use('/add-product', (req, res, next) => {
+  console.log('This Always runs');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('Add Product middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In the anothet middleware!');
   res.send('<h1>Hallo from Express</h1>');
 });
-
-// const server = http.createServer(app);
-// server.listen(3000);
 
 app.listen(3000);
