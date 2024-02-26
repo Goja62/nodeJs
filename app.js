ab7c4bd26848bba0b56a2242e15f7b16fe44685f
Goja <gortnar@beograd.com>
2024-02-26 11:25:28 +0100

    HANDLEBARS - Working with Handlabars

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const admminData = require('./routes/admin');
