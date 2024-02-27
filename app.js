14fe8afb408e8392f7899b0f38295af26c0a509f
Goja <gortnar@beograd.com>
2024-02-27 12:03:38 +0100

    EJS - Working with Ejs

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ const express = require('express');
 
 const path = require('path');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const admminData = require('./routes/admin');
