171d6cb7392f16c33a2815572b60e2ea56eb8cc6
Goja <gortnar@beograd.com>
2024-03-18 09:34:59 +0100

    sequelize - Deliting Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
   })
     .then((result) => {
       console.log(result);
+      res.redirect('/admin/products');
     })
     .catch((err) => {
       // console.log(err);
@@ -90,6 +91,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log('PRODUCT DESTOYED');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   res.redirect('/admin/products');
 };
