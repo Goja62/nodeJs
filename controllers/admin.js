fe9eef40aed61865c2756dbc9ac55e6a7fca4d0d
Goja <gortnar@beograd.com>
2024-03-20 10:20:20 +0100

    sequelize - Using Magic Assosiation Methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,19 +13,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      // userId: req.user.id,
+    })
     .then((result) => {
-      console.log(result);
+      // console.log(result);
+      console.log('Product Created');
       res.redirect('/admin/products');
     })
     .catch((err) => {
       // console.log(err);
-      console.log('Created Product');
     });
 };
 
