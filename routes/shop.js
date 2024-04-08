f4b1f56577921c2fd4b68a66541650481d906a45
Goja <gortnar@beograd.com>
2024-04-08 16:48:44 +0200

    Storing Multiple Products in the Cart -- A G A I N --

--- routes/shop.js
+++ routes/shop.js
@@ -12,8 +12,7 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
-q;
+// router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
