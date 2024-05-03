59accee037a7c32f54965dd1fd105bfd816c51de
Goja <gortnar@beograd.com>
2024-05-03 10:07:54 +0200

    VALIDATION - Validate Login Procrdure

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,14 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
@@ -88,30 +96,36 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
     });
   }
-
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPassword) => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect('/login');
-      return transporter
-        .sendMail({
-          to: email,
-          from: 'gortnar@beograd.com',
-          subject: 'Signup succsided',
-          html: '<h1>You successfully signd up!</h1>',
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash('error', 'E-mail exists alredy, please pick different one!');
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'gortnar@beograd.com',
+            subject: 'Signup succsided',
+            html: '<h1>You successfully signd up!</h1>',
+          });
         })
         .catch((err) => {
           console.log(err);
         });
     })
+
     .catch((err) => {
       console.log(err);
     });
--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,32 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email: ' + value + ' is forbiden!');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (!userDoc) {
+            return Promise.reject('Incorecct Email!');
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text an at least 5 characters'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 
 router.post('/logout', authController.postLogout);
 
