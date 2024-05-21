c2c01c2645c6a1f233269fd4b9001019908b356a
Goja <gortnar@beograd.com>
2024-05-21 15:20:29 +0200

    FILE UPLOAD & DOWNLOAD - Generating .pdf Files with Order Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -165,8 +165,24 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text('Hello World');
-
+      pdfDoc.fontSize(26).text('Invoice', { underline: true });
+      pdfDoc.text('--------------------------');
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(12)
+          .text(
+            prod.product.title +
+              ' - ' +
+              prod.quantity +
+              ' X ' +
+              '$' +
+              prod.product.price
+          );
+      });
+      pdfDoc.text('---------------------');
+      pdfDoc.fontSize(16).text('Total price: ' + totalPrice);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
